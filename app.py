db9fbf75c3843fb463f34474758c5e3015a15c20
sirria <36103216+sirria@users.noreply.github.com>
2018-10-26 14:15:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,8 +67,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     question = event.message.text
-    if(question == "hai"):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Hai juga"))
+    if(question == 'hai'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Hai juga'))
     else:
         answer = request_api(question)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=answer))
